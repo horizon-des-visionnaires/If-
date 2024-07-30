2ab65423b27a192d0ed51016cc8276c8f5af821a
RLUCAS <lucas.reboulet@ynov.com>
2024-07-30 13:05:35 +0200

    add search bar + dispaly user pro on page research

--- src/model/researchModel.php
+++ src/model/researchModel.php
@@ -21,4 +21,60 @@ class researchModel
         $this->dsn = new PDO("mysql:host=mysql;dbname=ifa_database", "ifa_user", "ifa_password");
         $this->dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
+
+    public function getFilteredProUsers($searchQuery = '', $sortBy = '', $order = 'DESC')
+    {
+        $query = $this->buildProUserQuery($searchQuery, $sortBy, $order);
+        $stmt = $this->dsn->prepare($query);
+
+        if ($searchQuery) {
+            $searchQuery = "%{$searchQuery}%";
+            $stmt->bindParam(':searchQuery', $searchQuery);
+        }
+
+        $stmt->execute();
+        $proUserData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($proUserData as &$user) {
+            $this->processUser($user);
+        }
+
+        return $proUserData;
+    }
+
+    private function buildProUserQuery($searchQuery, $sortBy, $order)
+    {
+        $query = "SELECT IdUser, FirstName, LastName, Email, ProfilPicture, ProfilDescription, ProfilPromotion 
+              FROM User 
+              WHERE IsPro = 1";
+
+        if ($searchQuery) {
+            $query .= " AND (FirstName LIKE :searchQuery 
+                    OR LastName LIKE :searchQuery)";
+        }
+
+        // switch ($sortBy) {
+        //     case 'name':
+        //         $query .= " ORDER BY FirstName $order, LastName $order";
+        //         break;
+        //     case 'promotion':
+        //         $query .= " ORDER BY ProfilPromotion $order";
+        //         break;
+        //     default:
+        //         $query .= " ORDER BY IdUser $order";
+        //         break;
+        // }
+
+        return $query;
+    }
+
+    private function processUserPicture($picture)
+    {
+        return $picture !== null ? base64_encode($picture) : '';
+    }
+
+    private function processUser(&$user)
+    {
+        $user['ProfilPicture'] = $this->processUserPicture($user['ProfilPicture']);
+    }
 }
